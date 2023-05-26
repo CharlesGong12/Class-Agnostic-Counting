b952f63d2daac123bf51d4eb7887a711f8a1b313
singularity-s0 <12184989+singularity-s0@users.noreply.github.com>
2023-05-26 11:51:58 +0800

    no mosiac

--- run.sh
+++ run.sh
@@ -3,22 +3,14 @@ set -e
 # *** reproduce results ***
 # see https://github.com/Verg-Avesta/CounTR/issues/23
 
-#python FSC_test_cross\(few-shot\).py --output_dir ./data/out/aaa --resume ./weights/FSC147.pth --box_bound 3
 
 
-# *** reproduce pre-training and fine-tuning described in the paper ***
-
+# To set proxy
 export http_proxy=http://127.0.0.1:7777 https_proxy=http://127.0.0.1:7777
 export socks_proxy=socks5://127.0.0.1:1080
 
-torchrun --standalone --nnodes=1 --nproc-per-node=8 FSC_finetune_cross.py --epochs 1000 --batch_size 8 --lr 1e-6 --output_dir ./data/out/finetune --title CLIP-TextOnly --resume FSC147.pth --blr 1e-4 --wandb CounTR
+# To run training
+torchrun --standalone --nnodes=1 --nproc-per-node=4 FSC_finetune_cross.py --epochs 1000 --batch_size 8 --lr 1e-6 --output_dir ./data/train/CLIP-TextOnly-NoMosiac --title CLIP-TextOnly-NoMosiac --resume FSC147.pth --blr 1e-4 --wandb CounTR
 
-# *** reproduce pre-training and fine-tuning described in the paper, using the large MAE model ***
-
-# CUDA_VISIBLE_DEVICES=0 python FSC_pretrain.py --epochs 300 --batch_size 16 --lr 5e-6 --output_dir ./data/out/pretrain_large --log_dir None --title CounTR_pretraining_paper_large --resume ./weights/mae_pretrain_vit_large_full.pth --model mae_vit_large_patch16
-# CUDA_VISIBLE_DEVICES=0 python FSC_finetune_cross.py --epochs 1000 --batch_size 8 --lr 1e-5 --output_dir ./data/out/finetune_large --log_dir None --title CounTR_finetuning_paper_large --resume ./data/out/pretrain_large/checkpoint__pretraining_299.pth --model mae_vit_large_patch16
-# python FSC_test_cross\(few-shot\).py --output_dir ./data/out/results_large --resume ./data/out/finetune_large/checkpoint__finetuning_minMAE.pth --box_bound 3 --model mae_vit_large_patch16
-
-
-
-CUDA_VISIBLE_DEVICES=0 python FSC_finetune_cross.py --epochs 1000 --batch_size 8 --lr 1e-5 --output_dir ./data/out/CLIP --log_dir None --title CounTR-CLIP --wandb CounTR-CLIP
+# To run testing
+python FSC_test_cross\(few-shot\).py --output_dir ./data/out/aaa --resume ./your-weights/FSC147.pth
--- util/FSC147.py
+++ util/FSC147.py
@@ -164,99 +164,16 @@ class ResizeTrainImage(ResizeSomeImage):
                 resized_density = TF.hflip(resized_density)
 
         # Random 384*384 crop in a new_W*384 image and 384*new_W density map
-        if mosaic_flag == 0:
-            if aug_flag == 0:
-                re_image = resized_image
-                resized_density = np.zeros((resized_density.shape[0], resized_density.shape[1]), dtype='float32')
-                for i in range(dots.shape[0]):
-                    resized_density[min(new_H - 1, int(dots[i][1]))][min(new_W - 1, int(dots[i][0] * scale_factor))] = 1
-                resized_density = torch.from_numpy(resized_density)
+        if aug_flag == 0:
+            re_image = resized_image
+            resized_density = np.zeros((resized_density.shape[0], resized_density.shape[1]), dtype='float32')
+            for i in range(dots.shape[0]):
+                resized_density[min(new_H - 1, int(dots[i][1]))][min(new_W - 1, int(dots[i][0] * scale_factor))] = 1
+            resized_density = torch.from_numpy(resized_density)
 
-            start = random.randint(0, new_W - 1 - 383)
-            reresized_image = TF.crop(re_image, 0, start, 384, 384)
-            reresized_density = resized_density[:, start:start + 384]
-        # Random self mosaic
-        else:
-            image_array = []
-            map_array = []
-            blending_l = random.randint(10, 20)
-            resize_l = 192 + 2 * blending_l
-            if dots.shape[0] >= 70:
-                for i in range(4):
-                    length = random.randint(150, 384)
-                    start_W = random.randint(0, new_W - length)
-                    start_H = random.randint(0, new_H - length)
-                    reresized_image1 = TF.crop(resized_image, start_H, start_W, length, length)
-                    reresized_image1 = transforms.Resize((resize_l, resize_l))(reresized_image1)
-                    reresized_density1 = np.zeros((resize_l, resize_l), dtype='float32')
-                    for i in range(dots.shape[0]):
-                        if start_H <= min(new_H - 1, int(dots[i][1])) < start_H + length and start_W <= min(new_W - 1, int(dots[i][0] * scale_factor)) < start_W + length:
-                            reresized_density1[min(resize_l-1,int((min(new_H-1,int(dots[i][1]))-start_H)*resize_l/length))][min(resize_l-1,int((min(new_W-1,int(dots[i][0]*scale_factor))-start_W)*resize_l/length))]=1
-                    reresized_density1 = torch.from_numpy(reresized_density1)
-                    image_array.append(reresized_image1)
-                    map_array.append(reresized_density1)
-            else:
-                m_flag = 1
-                prob = random.random()
-                if prob > 0.25:
-                    gt_pos = random.randint(0, 3)
-                else:
-                    gt_pos = random.randint(0, 4)  # 5% 0 objects
-                for i in range(4):
-                    if i == gt_pos:
-                        Tim_id = im_id
-                        r_image = resized_image
-                        Tdots = dots
-                        new_TH = new_H
-                        new_TW = new_W
-                        Tscale_factor = scale_factor
-                    else:
-                        Tim_id = self.train_set[random.randint(0, len(self.train_set) - 1)]
-                        Tdots = np.array(self.annotations[Tim_id]['points'])
-                        '''while(abs(Tdots.shape[0]-dots.shape[0]<=10)):
-                            Tim_id = train_set[random.randint(0, len(train_set)-1)]
-                            Tdots = np.array(annotations[Tim_id]['points'])'''
-                        Timage = Image.open('{}/{}'.format(self.im_dir, Tim_id))
-                        Timage.load()
-                        new_TH = 16 * int(Timage.size[1] / 16)
-                        new_TW = 16 * int(Timage.size[0] / 16)
-                        Tscale_factor = float(new_TW) / Timage.size[0]
-                        r_image = TTensor(transforms.Resize((new_TH, new_TW))(Timage))
-
-                    length = random.randint(250, 384)
-                    start_W = random.randint(0, new_TW - length)
-                    start_H = random.randint(0, new_TH - length)
-                    r_image1 = TF.crop(r_image, start_H, start_W, length, length)
-                    r_image1 = transforms.Resize((resize_l, resize_l))(r_image1)
-                    r_density1 = np.zeros((resize_l, resize_l), dtype='float32')
-                    if self.class_dict[im_id] == self.class_dict[Tim_id]:
-                        for i in range(Tdots.shape[0]):
-                            if start_H <= min(new_TH - 1, int(Tdots[i][1])) < start_H + length and start_W <= min(new_TW - 1, int(Tdots[i][0] * Tscale_factor)) < start_W + length:
-                                r_density1[min(resize_l-1,int((min(new_TH-1,int(Tdots[i][1]))-start_H)*resize_l/length))][min(resize_l-1,int((min(new_TW-1,int(Tdots[i][0]*Tscale_factor))-start_W)*resize_l/length))]=1
-                    r_density1 = torch.from_numpy(r_density1)
-                    image_array.append(r_image1)
-                    map_array.append(r_density1)
-
-            reresized_image5 = torch.cat((image_array[0][:, blending_l:resize_l-blending_l], image_array[1][:, blending_l: resize_l-blending_l]), 1)
-            reresized_density5 = torch.cat((map_array[0][blending_l:resize_l-blending_l], map_array[1][blending_l: resize_l-blending_l]), 0)
-            for i in range(blending_l):
-                    reresized_image5[:, 192+i] = image_array[0][:, resize_l-1-blending_l+i] * (blending_l-i)/(2 * blending_l) + reresized_image5[:, 192+i] * (i+blending_l)/(2*blending_l)
-                    reresized_image5[:, 191-i] = image_array[1][:, blending_l-i] * (blending_l-i)/(2*blending_l) + reresized_image5[:, 191-i] * (i+blending_l)/(2*blending_l)
-            reresized_image5 = torch.clamp(reresized_image5, 0, 1)
-
-            reresized_image6 = torch.cat((image_array[2][:, blending_l:resize_l-blending_l], image_array[3][:, blending_l: resize_l-blending_l]), 1)
-            reresized_density6 = torch.cat((map_array[2][blending_l:resize_l-blending_l], map_array[3][blending_l:resize_l-blending_l]), 0)
-            for i in range(blending_l):
-                    reresized_image6[:, 192+i] = image_array[2][:, resize_l-1-blending_l+i] * (blending_l-i)/(2*blending_l) + reresized_image6[:, 192+i] * (i+blending_l)/(2*blending_l)
-                    reresized_image6[:, 191-i] = image_array[3][:, blending_l-i] * (blending_l-i)/(2*blending_l) + reresized_image6[:, 191-i] * (i+blending_l)/(2*blending_l)
-            reresized_image6 = torch.clamp(reresized_image6, 0, 1)
-
-            reresized_image = torch.cat((reresized_image5[:, :, blending_l:resize_l-blending_l], reresized_image6[:, :, blending_l:resize_l-blending_l]), 2)
-            reresized_density = torch.cat((reresized_density5[:, blending_l:resize_l-blending_l], reresized_density6[:, blending_l:resize_l-blending_l]), 1)
-            for i in range(blending_l):
-                    reresized_image[:, :, 192+i] = reresized_image5[:, :, resize_l-1-blending_l+i] * (blending_l-i)/(2*blending_l) + reresized_image[:, :, 192+i] * (i+blending_l)/(2*blending_l)
-                    reresized_image[:, :, 191-i] = reresized_image6[:, :, blending_l-i] * (blending_l-i)/(2*blending_l) + reresized_image[:, :, 191-i] * (i+blending_l)/(2*blending_l)
-            reresized_image = torch.clamp(reresized_image, 0, 1)
+        start = random.randint(0, new_W - 1 - 383)
+        reresized_image = TF.crop(re_image, 0, start, 384, 384)
+        reresized_density = resized_density[:, start:start + 384]
 
         # Gaussian distribution density map
         reresized_density = ndimage.gaussian_filter(reresized_density.numpy(), sigma=(1, 1), order=0)
